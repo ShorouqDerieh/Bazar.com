3f56da575fed053c3e4dd4118696b8ba908c2fbc
HibaBanimenia <hiba.alzayed36@gmail.com>
2026-04-07 20:46:57 +0300

    adding logs

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,20 +1,40 @@
 from flask import Flask, jsonify
-from flask import jsonify #for json response
 import requests
+
 app = Flask(__name__)
+
 CATALOG_URL = "http://127.0.0.1:5000"
 ORDER_URL = "http://127.0.0.1:5001"
+
 @app.route("/search/<topic>")
 def search(topic):
+    print(f"Frontend: forwarding search request for topic '{topic}'")
+
     res = requests.get(f"{CATALOG_URL}/search/{topic}")
+
+    print(f"Frontend: received response from Catalog")
     return jsonify(res.json())
+
+
 @app.route("/info/<int:item_id>")
 def info(item_id):
+    print(f"Frontend: forwarding info request for book {item_id}")
+
     res = requests.get(f"{CATALOG_URL}/info/{item_id}")
+
+    print(f"Frontend: received response from Catalog")
     return jsonify(res.json())
+
+
 @app.route("/purchase/<int:item_id>", methods=["POST"])
 def purchase(item_id):
+    print(f"Frontend: forwarding purchase request for book {item_id}")
+
     res = requests.post(f"{ORDER_URL}/purchase/{item_id}")
+
+    print(f"Frontend: received response from Order")
     return jsonify(res.json())
+
+
 if __name__ == "__main__":
     app.run(port=5002, debug=True)
--- order/order.py
+++ order/order.py
@@ -1,9 +1,9 @@
 from flask import Flask, jsonify
-from flask import jsonify #for json response
 import sqlite3
 import requests
 
 app = Flask(__name__)
+
 conn = sqlite3.connect("orders.db")
 c = conn.cursor()
 
@@ -18,23 +18,41 @@ conn.commit()
 conn.close()
 
 CATALOG_URL = "http://127.0.0.1:5000"
+
 @app.route("/purchase/<int:item_id>", methods=["POST"])
 def purchase(item_id):
+    print(f"Order: received purchase request for book {item_id}")
     res = requests.get(f"{CATALOG_URL}/info/{item_id}")
+    print(f"Order: requesting book info from Catalog")
+
     if res.status_code != 200:
+        print(f"Order: book {item_id} not found")
         return jsonify({"error": "Book not found"}), 404
+
     book = res.json()
+
     if book["quantity"] <= 0:
+        print(f"Order: book {item_id} out of stock")
         return jsonify({"error": "Out of stock"}), 400
+
+    print(f"Order: updating Catalog (decreasing quantity)")
     update_res = requests.put(
         f"{CATALOG_URL}/update/{item_id}",
         json={"quantity": -1}
     )
+
     if update_res.status_code != 200:
+        print(f"Order: failed to update catalog")
         return jsonify({"error": "Failed to update catalog"}), 500
+
+    print(f"Order: saving order in database")
     conn = sqlite3.connect("orders.db", check_same_thread=False)
     c = conn.cursor()
+
     c.execute("INSERT INTO orders (item_id) VALUES (?)", (item_id,))
     conn.commit()
     conn.close()
+
+    print(f"Order: purchase completed for book {item_id}")
+
     return jsonify({"message": "Purchase successful"})
